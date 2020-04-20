1e1dd56259bd6c8787d9a91c802138dc8b9a1b58
Shunsuke <shun.sskd05@icloud.com>
2020-04-20 14:36:56 +0000

    [Add] userコントローラ、モデル”

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_124016) do
+ActiveRecord::Schema.define(version: 2020_04_20_143117) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
