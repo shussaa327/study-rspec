792517a856c2f6c718483f496a54744a4f964ce7
shunsuke sasaki <shun.sskd05@icloud.com>
2020-05-29 23:51:59 +0900

    [Add] rspec_test 計算

---
+++ spec/test_spec.rb
@@ -0,0 +1,12 @@
+describe "test" do
+  context "足し算" do
+    it "1 + 1が2になる" do
+      expect(1 + 1).to eq 2
+    end
+  end
+  context "足し算" do
+    it "1 + 1が2になる" do
+      expect(1 + 1).to eq 3
+    end
+  end
+end
